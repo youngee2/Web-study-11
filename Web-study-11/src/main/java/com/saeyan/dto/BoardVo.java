7b1984c2cdd4873cecf6f2b447b51175df3a5a14
Administrator <Administrator@User -2022UUTFS>
2022-09-01 12:54:29 +0900

    Initial commit Vo클래스 생성

---
+++ Web-study-11/src/main/java/com/saeyan/dto/BoardVo.java
@@ -0,0 +1,56 @@
+package com.saeyan.dto;
+
+import java.sql.Timestamp;
+
+public class BoardVo {
+	private int num;
+	private String name;
+	private String email;
+	private String pass;
+	private String title;
+	private int readcount;
+	private Timestamp writedate;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public Timestamp getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(Timestamp writedate) {
+		this.writedate = writedate;
+	}
+	
+}
